0ba7f25ffbafd8329c379f42cda3e0e22209f18c
mbuthi <mbuthimungai34@gmail.com>
2023-09-11 07:34:50 +0300

    Implement hero page

--- src/components/Collapsible/Collapsible.jsx
+++ src/components/Collapsible/Collapsible.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./Collapsible.css";
 
-const Collapsible = ({ children, headerContent, absolutePosition = false }) => {
+const Collapsible = ({ children, headerContent, absolutePosition = false, bordered = false }) => {
     const [isOpen, setIsOpen] = useState(false);
 
     const contentStyle = {
@@ -12,12 +12,15 @@ const Collapsible = ({ children, headerContent, absolutePosition = false }) => {
     return (
         <div className="collapsible-container">
             <div
-                className={`collapsible ${isOpen ? 'active' : ''}`}
+                className={`collapsible flex ${bordered ? 'bordered' : ''} ${isOpen ? 'active' : ''}`}
                 onClick={() => setIsOpen(!isOpen)}
             >
                 {headerContent}
+                <span className="rightHC">
+                                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="sc-iCoGMd iZDSCZ"><path d="M8.71005 11.71L11.3001 14.3C11.6901 14.69 12.3201 14.69 12.7101 14.3L15.3001 11.71C15.9301 11.08 15.4801 10 14.5901 10H9.41005C8.52005 10 8.08005 11.08 8.71005 11.71Z"></path></svg>
+                                    </span>
             </div>
-            <div className="content" style={contentStyle}>
+            <div className={`content ${bordered ? 'bordered' : ''}`} style={contentStyle}>
                 {children}
             </div>
         </div>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -54,12 +54,13 @@ const Hero = () => {
                                         <span className="spanSortBy">Multiplier</span>
                                     </div>                                    
                                     <span>
-                                        <svg viewBox="0 0 24 24" color="text" width="20px" xmlns="http://www.w3.org/2000/svg" class="sc-iCoGMd iZDSCZ"><path d="M8.71005 11.71L11.3001 14.3C11.6901 14.69 12.3201 14.69 12.7101 14.3L15.3001 11.71C15.9301 11.08 15.4801 10 14.5901 10H9.41005C8.52005 10 8.08005 11.08 8.71005 11.71Z"></path></svg>
+                                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="sc-iCoGMd iZDSCZ"><path d="M8.71005 11.71L11.3001 14.3C11.6901 14.69 12.3201 14.69 12.7101 14.3L15.3001 11.71C15.9301 11.08 15.4801 10 14.5901 10H9.41005C8.52005 10 8.08005 11.08 8.71005 11.71Z"></path></svg>
                                     </span>
                                 </div>
                             } 
                             
                             absolutePosition={true}
+                            bordered={true}
                             >
                                 <ul className="sortUl">
                                     <li>Hot</li>
@@ -68,8 +69,11 @@ const Hero = () => {
                                     <li>Liquidity</li>
                                 </ul>
                             </Collapsible>
-                        </div>                  
-                        <Input placeholder="Search Farms"/>
+                        </div>  
+                        <div id="inputHero">
+                            <Input placeholder="Search Farms"/>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
